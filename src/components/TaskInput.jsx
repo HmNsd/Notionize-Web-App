c760a1cacfc98a21fd118e954d104ef3184179dc
Himanshu Nishad <himanshunishadofficial@gmail.com>
2025-09-06 01:22:46 +0530

    empty task add test case handled

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -10,11 +10,16 @@ function TodoForm() {
 
   const add = (e) => {
     e.preventDefault();
-
+// (  : setTodo(e.target.value))
     if (!todo) return;
 
+    else if(todo.trim()==false){
+      window.alert("Task can't be empty")
+    }
+    else{
+
     addTodo({ todo, completed: false });
-    setTodo("");
+    setTodo("");}
   };
 
   return (
